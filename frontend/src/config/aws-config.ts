bc40fb0c55bda8ad95bd7545edbd521126f48e0b
Connorrmcd6 <connormcd98@gmail.com>
2025-10-17 13:19:15 +0200

    remove packages

--- frontend/src/config/aws-config.ts
+++
@@ -1,6 +0,0 @@
-export const awsConfig = {
-  region: 'af-south-1',
-  userPoolId: 'af-south-1_bvWGcrrvu',
-  userPoolWebClientId: '33dje318brmcug2sk8sgif4imu',
-  authenticationFlowType: 'USER_PASSWORD_AUTH',
-};
